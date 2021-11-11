5ab756cbc3b61ad0b7e755bf1d5ad20896209398
Fabro0 <47898082+Fabro0@users.noreply.github.com>
2021-11-11 20:55:26 -0300

    Hola Mundo!

--- app/src/main/java/com/example/gallurca_the_remix/Casa.java
+++ app/src/main/java/com/example/gallurca_the_remix/Casa.java
@@ -3,6 +3,7 @@ package com.example.gallurca_the_remix;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,11 +11,26 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 //import android.support.v7.app.AppCompatActivity;
 
 public class Casa extends AppCompatActivity {
     TextView timerTxt;
     private long startTime;
+    private FirebaseFirestore db;
+    private FirebaseAuth firebaseUser;
+    private FirebaseUser user;
+
 
     Handler timerHandler = new Handler();
     Runnable timerRunnable = new Runnable() {
@@ -33,12 +49,45 @@ public class Casa extends AppCompatActivity {
     };
 
     public void guardar(View view){
-        String mensaje = "Tiempo: " + timerTxt.getText().toString();
-        Toast.makeText(Casa.this, mensaje, Toast.LENGTH_LONG).show();
-    }
+        String uid = user.getUid();
+        Log.d("UID", uid);
+        ArrayList<String> time = new ArrayList<String>();
+        time.add(timerTxt.getText().toString());
 
+        Map<String, Object> map = new HashMap<>();
+        map.put("lastTimes", FieldValue.arrayUnion(time));
+
+        Log.d("mapa", map.toString());
+
+        db.collection("users").document(uid).update("lastTimes", FieldValue.arrayUnion(timerTxt.getText().toString()))
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(Casa.this, "SAVED", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+
+
+        }
+    public void guardados(View view){
+        String uid = user.getUid();
+
+        db.collection("users").document(uid).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshots) {
+
+                        Toast.makeText(Casa.this, documentSnapshots.get("lastTimes").toString(), Toast.LENGTH_LONG).show();
+
+                    }});
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance();
+        user = firebaseUser.getCurrentUser();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_casa);
         timerTxt =  (TextView) findViewById(R.id.timerTxt);
